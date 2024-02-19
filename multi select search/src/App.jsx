03fa6b0608ebfc3008d01a82466dbeeebfc5f6f8
akashprajapati20 <92972992+akashprajapati20@users.noreply.github.com>
2024-02-19 19:55:50 +0530

    almost completed

--- multi select search/src/App.jsx
+++ multi select search/src/App.jsx
@@ -1,12 +1,16 @@
 
-import { useEffect, useState } from 'react'
+import { useEffect,useRef, useState } from 'react'
 import './App.css'
+import Pills from "./components/Pills";
 
 function App() {
    const[inpVal,setInpVal]=useState("");
    const [suggestion,setSuggestion]=useState([]);
    const [selectedUsers,setSelectedUsers]=useState([]);
+   const [selectedUserSet, setSelectedUserSet] = useState(new Set());
+   const [activeSuggestion, setActiveSuggestion] = useState(0);
 
+   const inputRef = useRef(null);
 
    useEffect(()=>{
     async function searchusers(){
@@ -25,21 +29,67 @@ function App() {
 
    const  handleSelectusers=(user)=>{
     setSelectedUsers([...selectedUsers,user]);
+    setSelectedUserSet(new Set([...selectedUserSet, user.email]));
     setInpVal("");setSuggestion([]);
-    console.log(selectedUsers);
+    // console.log(selectedUsers);
+    inputRef.current.focus();
    }
-  
-    
+  const handleRemove=(user)=>{
+    const updatedUsers = selectedUsers.filter(
+      (selectedUser) => selectedUser.id !== user.id
+    );
+    setSelectedUsers(updatedUsers);
+
+    const updatedEmails = new Set(selectedUserSet);
+    updatedEmails.delete(user.email);
+    setSelectedUserSet(updatedEmails);
+  }
+  const handleKeyDown = (e) => {
+    if (
+      e.key === "Backspace" &&
+      e.target.value === "" &&
+      selectedUsers.length > 0
+    ) {
+      const lastUser = selectedUsers[selectedUsers.length - 1];
+      handleRemove(lastUser);
+      setSuggestion([]);
+    } else if (e.key === "ArrowDown" && suggestion?.users?.length > 0) {
+      e.preventDefault();
+      setActiveSuggestion((prevIndex) =>
+        prevIndex < suggestion.users.length - 1 ? prevIndex + 1 : prevIndex
+      );
+    } else if (e.key === "ArrowUp" && suggestion?.users?.length > 0) {
+      e.preventDefault();
+      setActiveSuggestion((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : 0));
+    } else if (
+      e.key === "Enter" &&
+      activeSuggestion >= 0 &&
+      activeSuggestion < suggestion.users.length
+    ) {
+      handleSelectusers(suggestion.users[activeSuggestion]);
+    }
+  };
+
   return (
     <>
       <div className="container">
         <div className="search">
-          <input value={inpVal} onChange={(e)=>{setInpVal(e.target.value)}} type="text" placeholder="search for users..." />
+          <div className="selectedusers">
+
+          {
+            selectedUsers.map((user)=>{
+              return (
+                <Pills   key={user.email} image={user.image}   text={`${user.firstName} ${user.lastName}`}   onClick={()=>handleRemove(user)}/>
+              )
+            })
+          }
+          </div>
+          <input onKeyDown={handleKeyDown}  ref={inputRef} value={inpVal} onChange={(e)=>{setInpVal(e.target.value)}} type="text" placeholder="search for users..." />
           
         <div className="suggestions-list">
             <ul>
             {suggestion?.users?.map((user)=>{
-              return  (
+              return !selectedUserSet.has(user.email)? (
                 <li
                 key={user.email}
                 onClick={()=>handleSelectusers(user)}
@@ -47,7 +97,7 @@ function App() {
                   <img src={user.image} alt={`${user.firstName} ${user.lastName}`} />
                  <span>{user.firstName} {user.lastName}</span> 
                 </li>
-              ); 
+              ):<></>; 
             })}
             </ul>
         </div>
